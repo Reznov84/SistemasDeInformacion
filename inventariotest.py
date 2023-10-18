4be5680417f77b1e88b830b1dac666b11131f7d9
Eduardo <yayorubi@hotmail.com>
2023-10-17 21:34:53 -0600

    correciones

--- inventariotest.py
+++ inventariotest.py
@@ -1,40 +1,40 @@
 import csv
 
-# Cargar el inventario desde el archivo CSV
+#cargar el inventario 
 inventario = {}
 with open("inventario.csv", "r") as file:
     csv_reader = csv.reader(file)
-    next(csv_reader)  # Saltar la fila de encabezados
+    next(csv_reader)  # Saltar la primera fila
     for row in csv_reader:
         producto, cantidad = row
         inventario[producto] = int(cantidad)
 
-# Definir las recetas de cafe
+#recetas de cafe
 recetas = {
     "Cafe Americano": {"Agua": 500, "Cafe": 10, "Vasos": 1},
     "Cafe Latte": {"Agua": 300, "Leche": 200, "Cafe": 15, "Vasos": 1},
     "Cafe Espresso": {"Agua": 100, "Cafe": 15, "Vasos": 1},
 }
 
-# Función para sustraer ingredientes del inventario
+#quitar ingredientes del inventario
 def sustraer_ingredientes(receta, cantidad):
     for ingrediente, cantidad_necesaria in recetas[receta].items():
         inventario[ingrediente] -= cantidad_necesaria * cantidad
 
-# Función para verificar si hay suficientes ingredientes
+#verificar si hay suficientes ingredientes
 def verificar_disponibilidad(receta, cantidad):
     for ingrediente, cantidad_necesaria in recetas[receta].items():
         if inventario[ingrediente] < cantidad_necesaria * cantidad:
             return False
     return True
 
-# Función para mostrar el inventario
+#mostrar el inventario
 def mostrar_inventario():
     print("Inventario:")
     for producto, cantidad in inventario.items():
         print(f"{producto}: {cantidad}")
 
-# Menú para el usuario
+#menu para el usuario
 while True:
     print("\nMenú de Cafetería:")
     for cafe in recetas:
@@ -56,12 +56,12 @@ while True:
     else:
         print("Café no válido. Por favor, elige una opción válida.")
 
-# Guardar los cambios en el inventario
+#guardar los cambios en el inventario
 with open("inventario.csv", "w", newline="") as file:
     csv_writer = csv.writer(file)
     csv_writer.writerow(["Producto", "Cantidad"])
     for producto, cantidad in inventario.items():
         csv_writer.writerow([producto, cantidad])
 
-# Mostrar el inventario actualizado
+#mostrar el inventario actualizado
 mostrar_inventario()
